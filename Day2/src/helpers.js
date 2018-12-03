3b6fd2489fdd11eb075da6155d71adbaba378cbe
Michal Karbownik <michakarbownik@gmail.com>
2018-12-03 08:57:11 +0100

    Simplified and optimised the code

--- Day2/src/helpers.js
+++ Day2/src/helpers.js
@@ -9,34 +9,34 @@ function getOccurrences(idsArray) {
     //for all IDs in the array
     for(let counter = 0; counter < idsArray.length; counter++) {
         let currentID = idsArray[counter];
-        let letterOccurrenceArray = [];
+        let letterOccurrenceMap = new Map();
         let currentCharacter;
 
         //for all characters in the ID
         for (let characterCounter = 0; characterCounter < currentID.length; characterCounter++) {
             currentCharacter = currentID.charAt(characterCounter);
+            let currentNumberOfOccurrences;
 
-            //if the array record exists it will return the number of occurrences, if it doesn't it will return 'undefined'
-            let currentNumberOfOccurrences = letterOccurrenceArray[currentCharacter];
-
-            //if it is defined - increment, if undefined - assign the first occurrence
-            letterOccurrenceArray[currentCharacter] = currentNumberOfOccurrences ? currentNumberOfOccurrences + 1 : 1;
-        }
-
-        //iterate through the array and check if any of the letters occurs twice
-        for (currentCharacter in letterOccurrenceArray) {
-            if (letterOccurrenceArray[currentCharacter] === 2) {
-                hadTwoOccurrences++;
-                break;
+            //if there is a record with the current character as a key, increment its value, else add it to the mao with a value of 1
+            if((currentNumberOfOccurrences = letterOccurrenceMap.get(currentCharacter)) !== undefined) {
+                letterOccurrenceMap.set(currentCharacter, ++currentNumberOfOccurrences);
+            }
+            else {
+                letterOccurrenceMap.set(currentCharacter, 1);
             }
         }
 
-        //iterate through the array and check if any of the letters occurs three times
-        for (currentCharacter in letterOccurrenceArray) {
-            if (letterOccurrenceArray[currentCharacter] === 3) {
-                hadThreeOccurrences++;
-                break;
-            }
+        //convert the map to array
+        let letterOccurrenceArray = Array.from(letterOccurrenceMap.values());
+
+        //check if any of the letters occurs twice and increment if positive
+        if (letterOccurrenceArray.includes(2)) {
+            hadTwoOccurrences++;
+        }
+
+        //check if any of the letters occurs three times and increment if positive
+        if (letterOccurrenceArray.includes(3)) {
+            hadThreeOccurrences++;
         }
     }
 
