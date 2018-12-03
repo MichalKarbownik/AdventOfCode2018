a7b0857b1b6b0f2f52c693838d704558ead5ee53
Michal Karbownik <michakarbownik@gmail.com>
2018-12-03 19:22:50 +0100

    Solved first part of the Day 3

---
+++ Day3/src/helpers.js
@@ -0,0 +1,55 @@
+function parseData(inputData) {
+    inputData = inputData.split("\n");
+
+    //set a regular expression that retrieves numbers
+    let regex = /([0-9]*)\d/g;
+
+    //parse input data to contain numerical data from the strings in the form of array of arrays
+    return inputData.map(element => {
+        return element.match(regex);
+    });
+}
+
+function fillMaterialField(inputDataArray) {
+    //initialize a 1000x1000 array
+    let materialField = [...Array(1000)].map(() => Array(1000));
+
+    //for each material claim
+    inputDataArray.forEach((claim) => {
+        let startingXIndex = parseInt(claim[1]);
+        let startingYIndex = parseInt(claim[2]);
+        let finishingXIndex = startingXIndex + parseInt(claim[3]);
+        let finishingYIndex = startingYIndex + parseInt(claim[4]);
+
+        //on each square inch from starting indexes to finishing indexes in both X and Y axis, increment number of claims
+        for(let xCounter = startingXIndex; xCounter < finishingXIndex; xCounter++) {
+            for(let yCounter = startingYIndex; yCounter < finishingYIndex; yCounter++) {
+                if(!materialField[xCounter][yCounter]) {
+                    materialField[xCounter][yCounter] = 1;
+                }
+                else {
+                    materialField[xCounter][yCounter]++;
+                }
+            }
+        }
+    });
+
+    return materialField;
+}
+
+export function getNumberOfInchesOverlapped(inputData) {
+    let materialField = fillMaterialField(parseData(inputData));
+    let inchesOverlapped = 0;
+
+    //iterate through the whole material
+    materialField.forEach(materialRows => {
+        materialRows.forEach(materialColumns => {
+            //if there was more than one claim, increment an overlap counter
+            if(materialColumns > 1) {
+                inchesOverlapped++;
+            }
+        });
+    });
+
+    return inchesOverlapped;
+}
---
+++ Day3/src/main.js
@@ -0,0 +1,4 @@
+import { INPUT_DATA_PART_ONE } from "./data";
+import { getNumberOfInchesOverlapped } from "./helpers";
+
+console.log("Answer to the first part is: " + getNumberOfInchesOverlapped(INPUT_DATA_PART_ONE));
