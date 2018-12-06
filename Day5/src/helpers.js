cb0e5cb770be8c9737921c4c4a5993d15136b881
Michal Karbownik <michakarbownik@gmail.com>
2018-12-06 21:24:55 +0100

    Solved second part of the Day 5

--- Day5/src/helpers.js
+++ Day5/src/helpers.js
@@ -26,3 +26,58 @@ export function getRemainingNumberOfUnits(inputData) {
 
     return polymerAfterReaction.length;
 }
+
+function getOppositePolarity(unit) {
+    //if it's upper case convert to lower case
+    if (unit === unit.toUpperCase()) {
+        return unit.toLowerCase();
+    }
+    //else convert to upper case
+    else {
+        return unit.toUpperCase();
+    }
+}
+
+function deleteFaulty(polymer) {
+    let deletedCharacterToPolymerMap = new Map();
+
+    for(let characterCounter = 0; characterCounter < polymer.length; characterCounter++) {
+        //gets current character
+        let currentCharacter = polymer.charAt(characterCounter);
+        //gets current character's opposite case
+        let currentCharacterOppositeCase = getOppositePolarity(currentCharacter);
+        //creates a unique checksum for both characters
+        let characterChecksum = currentCharacter.charCodeAt(0) + currentCharacterOppositeCase.charCodeAt(0);
+
+        //if characters haven't been deleted, do it
+        if(deletedCharacterToPolymerMap.get(characterChecksum) === undefined) {
+            let polymerCopy = polymer;
+
+            let regex = new RegExp(currentCharacter, "g");
+            polymerCopy = polymerCopy.replace(regex, '');
+            regex = new RegExp(currentCharacterOppositeCase, "g");
+            polymerCopy = polymerCopy.replace(regex, '');
+
+            deletedCharacterToPolymerMap.set(characterChecksum, conductReaction(polymerCopy))
+        }
+    }
+
+    return deletedCharacterToPolymerMap;
+}
+
+function getMinimumPolymer(deletedCharacterToPolymerMap) {
+    let minimumPolymerCharPair = Array.from(deletedCharacterToPolymerMap).reduce((previousElement, currentElement) => {
+        return (previousElement[1].length < currentElement[1].length) ? previousElement : currentElement;
+    });
+
+    return minimumPolymerCharPair[1];
+}
+
+export function getAnswerForSecondPart(inputData) {
+    let polymerAfterReaction = conductReaction(inputData);
+
+    let resultingPolymerMap = deleteFaulty(polymerAfterReaction);
+    return getMinimumPolymer(resultingPolymerMap).length;
+}
+
+
--- Day5/src/main.js
+++ Day5/src/main.js
@@ -1,4 +1,5 @@
 import { INPUT_DATA } from "./data";
-import { getRemainingNumberOfUnits } from "./helpers";
+import { getRemainingNumberOfUnits, getAnswerForSecondPart } from "./helpers";
 
 console.log("Answer to the first part is: " + getRemainingNumberOfUnits(INPUT_DATA));
+console.log("Answer to the second part is: " + getAnswerForSecondPart(INPUT_DATA));
